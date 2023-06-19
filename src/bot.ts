ee12a86eab52fcbc9ff1f1c8636aa9879ce64bb1
theKozman <markozyk@gmail.com>
2023-06-19 22:12:00 +0300

    add detection checks

--- src/bot.ts
+++ src/bot.ts
@@ -10,6 +10,8 @@ const ld = new LanguageDetect();
 const config = {
   ruMaxIndex: 2, // how far down russian should be down possible languages list to be considered detected (starts from 0)
   delta: 1.5, // how different ru and ua weights should be to be considered detected
+  precisionPercent: 90, // how precise guess should be to pass
+  minScore: 500, // lowest guess score to pass
 };
 
 /**
@@ -20,79 +22,6 @@ const config = {
 bot.command(ECommands.START, (ctx) => {
   ctx.reply('Бот для розпізнавання і негайного видаляння повідомлень російською в телеграм групах. Для початку роботи потрібно додати бота в групу і надати йому права адміна');
 });
-
-// bot.on('message:text', async (ctx) => {
-//   // TODO: only direct messages from users
-//   const reject = async (msg: string, data?: TStatRecord) => {
-//     if (data) {
-//       data.rejectReason = msg;
-//       await createStatsRecord(data);
-//     }
-//     await ctx.reply(`Russian not detected, reason: ${msg}`, { reply_to_message_id: ctx.msg.message_id });
-//   };
-
-//   const detection = ld.detect(String(ctx.message.text)) || [];
-//   if (detection.length === 0) return await reject(`Can't detect any language`);
-
-//   const ru = detection.find((l) => l[0] === ELangs.ru);
-//   const ua = detection.find((l) => l[0] === ELangs.ua);
-
-//   if (!ru?.length || !ua?.length) return await reject(`${ELangs.ru} or ${ELangs.ua} not found`);
-
-//   const ruIndex = detection.indexOf(ru);
-//   const uaIndex = detection.indexOf(ua);
-
-//   if (ruIndex > uaIndex)
-//     return reject(`${ELangs.ua}(${uaIndex}) has higher index than ${ELangs.ru}(${ruIndex})`, {
-//       messageId: ctx.message.message_id,
-//       ruWeight: ru[1],
-//       ruIndex,
-//       uaWeight: ua[1],
-//       uaIndex,
-//       message: ctx.message.text,
-//       rejected: true,
-//       config,
-//     });
-
-//   if (ruIndex > config.ruMaxIndex)
-//     return reject(`${ELangs.ru} index is too low (${ruIndex})`, {
-//       messageId: ctx.message.message_id,
-//       ruWeight: ru[1],
-//       ruIndex,
-//       uaWeight: ua[1],
-//       uaIndex,
-//       message: ctx.message.text,
-//       rejected: true,
-//       config,
-//     });
-
-//   const delta = ru[1] / ua[1];
-
-//   if (delta < config.delta)
-//     return reject(`Delta is too small (${delta})`, {
-//       messageId: ctx.message.message_id,
-//       ruWeight: ru[1],
-//       ruIndex,
-//       uaWeight: ua[1],
-//       uaIndex,
-//       message: ctx.message.text,
-//       rejected: true,
-//       config,
-//     });
-
-//   await createStatsRecord({
-//     messageId: ctx.message.message_id,
-//     ruWeight: ru[1],
-//     ruIndex,
-//     uaWeight: ua[1],
-//     uaIndex,
-//     message: ctx.message.text,
-//     rejected: false,
-//     config,
-//   });
-
-//   await ctx.reply(`russian detected\nruIndex: ${ruIndex};\nuaIndex: ${uaIndex}\nruWeight ${ru[1]};\nuaWeight: ${ua[1]}\ndelta: ${delta};`, { reply_to_message_id: ctx.msg.message_id });
-// });
 
 bot.on('message:text', async (ctx) => {
   const reject = async (msg: string, data?: TStatRecord) => {
@@ -109,8 +38,24 @@ bot.on('message:text', async (ctx) => {
   } catch (err) {
     return await reject(`Can't detect any language`);
   }
+
+  const ru = detection.languages.find((l) => l.name === ELangs.ru);
+  const ua = detection.languages.find((l) => l.name === ELangs.ua);
+
+  // Check if russian is detected
+  if (!ru) return await reject(`${ELangs.ru} not found`);
+
+  // Check if message has both ua and ru
+  if (ru && ua) return await reject(`Both ${ELangs.ru} and ${ELangs.ua} detected`);
+
+  // Check if precision percent is too low
+  if (ru.percent < config.precisionPercent) return await reject(`${ELangs.ru} precision percent is too low`);
+
+  // Check if score is high enough
+  if (ru.score < config.minScore) return await reject(`${ELangs.ru} guess score is too low`);
+
   console.log(detection);
-  await ctx.reply(`detected language: ${detection?.languages[0].name};\nreliable: ${detection?.reliable};\npercent: ${detection?.languages[0].percent};\nscore: ${detection?.languages[0].score}`, { reply_to_message_id: ctx.msg.message_id });
+  await ctx.reply(`detected language: ${ru.name};\nreliable: ${detection?.reliable};\npercent: ${detection?.languages[0].percent};\nscore: ${detection?.languages[0].score}`, { reply_to_message_id: ctx.msg.message_id });
 });
 
 bot.start();
--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,4 +1,4 @@
 export enum ELangs {
-  ru = 'russian',
-  ua = 'ukrainian',
+  ru = 'RUSSIAN',
+  ua = 'UKRAINIAN',
 }
