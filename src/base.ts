a9e1dc89c56307688e1af5af8650212ca35e9124
theKozman <markozyk@gmail.com>
2023-06-21 09:48:18 +0300

    cleanup

--- src/base.ts
+++ src/base.ts
@@ -1,22 +1,22 @@
-import { Deta } from 'deta';
-import { TStatRecord } from './types';
+// import { Deta } from 'deta';
+// import { TStatRecord } from './types';
 
-const deta = Deta();
+// const deta = Deta();
 
-export const db = deta.Base('rusoriz_db');
+// export const db = deta.Base('rusoriz_db');
 
-export const createStatsRecord = async (data: TStatRecord) => {
-  const { ...parsedData } = data;
-  try {
-    await db.put(parsedData);
-  } catch (err) {
-    //TODO: Error handling
-    console.error(err);
-    return err;
-  }
-};
+// export const createStatsRecord = async (data: TStatRecord) => {
+//   const { ...parsedData } = data;
+//   try {
+//     await db.put(parsedData);
+//   } catch (err) {
+//     //TODO: Error handling
+//     console.error(err);
+//     return err;
+//   }
+// };
 
-export const listAllRecords = async () => {
-  const { items } = await db.fetch();
-  return items;
-};
+// export const listAllRecords = async () => {
+//   const { items } = await db.fetch();
+//   return items;
+// };
