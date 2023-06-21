6d2a1bb32813d92bc2f9b3f329a1770005e4bd97
theKozman <markozyk@gmail.com>
2023-06-21 13:35:22 +0300

    remove scripts

--- src/scripts/index.ts
+++
@@ -1,7 +0,0 @@
-import { listAllRecords } from '../base';
-import fs from 'fs';
-
-(async () => {
-  const result = await listAllRecords();
-  fs.writeFile('./output.json', JSON.stringify(result), () => {});
-})();
