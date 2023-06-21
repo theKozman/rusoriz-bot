43079f7105f00abc7e4d08cb62d5797abffbd640
theKozman <markozyk@gmail.com>
2023-06-21 21:27:15 +0300

    testing config permissions

--- src/middleware/callbacks.ts
+++ src/middleware/callbacks.ts
@@ -7,8 +7,13 @@ export const configCallback = new Composer<TCustomContext>();
 configCallback.on('callback_query:data', async (ctx) => {
   // TODO: make only user that added bot able to do that and add permissions?
   // make sure that only i can change config in gachistan
-  console.log(ctx.chat?.id, ctx.from.id);
-  if (ctx.chat?.id === ID.gachistan && ctx.from.id !== ID.kozman) return;
+  console.log(ctx.chat?.id === ID.gachistan);
+  console.log(ctx.from?.id !== ID.kozman);
+  if (ctx.chat?.id === ID.gachistan && ctx.from.id !== ID.kozman) {
+    return await ctx.answerCallbackQuery({
+      text: EPhrases.MODE_PERMISSION_DENIED,
+    });
+  }
   ctx.session.onDetectMode = ctx.callbackQuery.data as EDetectionModes;
   await ctx.answerCallbackQuery({
     text: EPhrases.MODE_CHANGED,
