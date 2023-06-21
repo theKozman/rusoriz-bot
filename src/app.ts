1fff7df61ae3c4e17acc6a53f30dc621768a812a
theKozman <markozyk@gmail.com>
2023-06-21 09:45:48 +0300

    cleanup

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,7 @@
 import 'dotenv/config';
-import { Bot, webhookCallback } from 'grammy';
-import LanguageDetect from 'languagedetect';
-import express from 'express';
+import { Bot } from 'grammy';
 import { TCustomContext } from './types';
 
 const port = process.env.PORT || 3000;
 
 export const bot = new Bot<TCustomContext>(String(process.env.BOT_TOKEN));
-
-export const server = express();
-
-export const main = () => {
-  //server middlewares
-  server.use(express.json());
-  server.get('/', (req, res) => res.send('<h1>welcome!</h1>'));
-  server.use(webhookCallback(bot, 'express'));
-
-  server.listen(port, async () => {
-    console.log(`server is listening to port ${port}`);
-
-    const webhookUrl = `https://${process.env.DETA_SPACE_APP_HOSTNAME}`;
-
-    await bot.api.deleteWebhook();
-    await bot.api.setWebhook(webhookUrl, {
-      drop_pending_updates: true,
-    });
-  });
-};
