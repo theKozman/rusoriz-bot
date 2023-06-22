3efcc607ed282d858d957afbacc0f7e2a7ade012
theKozman <markozyk@gmail.com>
2023-06-22 23:26:41 +0300

    update config score

--- src/config.ts
+++ src/config.ts
@@ -2,5 +2,5 @@ export const config = {
   ruMaxIndex: 2, // how far down russian should be down possible languages list to be considered detected (starts from 0)
   delta: 1.5, // how different ru and ua weights should be to be considered detected
   precisionPercent: 90, // how precise guess should be to pass
-  minScore: 500, // lowest guess score to pass
+  minScore: 450, // lowest guess score to pass
 };
